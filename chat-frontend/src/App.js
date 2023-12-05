3fd8bc3fcc982aee983a8e310a56324e0acbc99c
LalitAswal <Lalit.Aswal143@gmail.com>
2023-12-05 22:28:09 +0530

    react-structure

--- chat-frontend/src/App.js
+++ chat-frontend/src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from "react";
-import { Route, Redirect, Switch } from "react-router-dom";
-import "./App.css";
-// the below components will be created shortly
-import Login from "./components/Login";
-import Groupchat from "./components/Groupchat";
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-render() {
-    return (
-      <Switch>
-        <Redirect exact from="/" to="/login" />
-        <Route path="/login" component={Login} />
-        <Route path="/chat" component={Groupchat} />
-      </Switch>
-    );
-  }
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
 export default App;
